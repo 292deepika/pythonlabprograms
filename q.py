9ab8d7b483e2aa32f8f823a0b89ede885b869bb6
292deepika <48008145+292deepika@users.noreply.github.com>
2019-02-28 15:03:31 +0530

    Add files via upload

--- q.py
+++ q.py
@@ -1,22 +1,38 @@
 import random
 l=["r","p","s"]
+d = {"r": "rock","p":"paper","s":"scissors"}
+us=0
+cs=0
 while True:
-	u=input("enter your choice,press n to discontinue")
+	u=input("enter your choice,\npress n to discontinue\n user chooses ")
 	if u=='n':
-		print("Game over")
+		print("Game over","user score =",us,"comp score =",cs )
+		if us>cs:
+			print("user wins")
+		if cs>us:
+			print("comp wins")	
 		exit()
 	c=random.choice(l)
-	print("computer chooses",c)
+	print("computer chooses",d[c])
+	print("user chooses",d[u])
 	if u ==c:
 		print("tie")
 	elif u=='r' and c=='p':
 		print("comp wins")
+		cs=cs+1
 	elif u== 'r'and c=='s':
 		print("user wins")
+		us=us+1
 	elif u=='s' and c=='p':
 		print("comp wins")
+		cs=cs+1
 	elif u=='p' and c=='s':
 		print("comp wins")
+		cs=cs+1
 	elif u=='p'and c=='r':
-		print("user wins")				
+		print("user wins")
+		us=us+1
+	elif u=='s'and c=='r':
+		print("comp wins")
+		cs=cs+1				
 
